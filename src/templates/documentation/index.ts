c0a67da731364f7f17fef90013d5aa0ecd286f17
victo-dev-qriarlabs <victohugo.pessoal@gmail.com>
2025-09-29 15:15:30 -0300

    docs: remove direct mode

--- src/templates/documentation/index.ts
+++ src/templates/documentation/index.ts
@@ -4,6 +4,4 @@ export const documentationTemplate = `
 **Description**
 Provide a summary of the changes made to the documentation. Indicate which sections were added, updated, or removed.
 
-**Motivation**
-Explain why the documentation needed to be updated. It could be to reflect new features, correct outdated information, or improve clarity.
 `;
